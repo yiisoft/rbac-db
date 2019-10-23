ace7bfba5cf09494cc679a30b23663e51e80c9c3
Alexander Makarov <sam@rmcreative.ru>
2019-10-23 10:57:27 +0300

    Moved files from general RBAC library

---
+++ src/SqliteManagerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Yiisoft\Rbac\Tests;
+
+/**
+ * SqliteManagerTest.
+ *
+ * @group db
+ * @group rbac
+ * @group sqlite
+ */
+class SqliteManagerTest extends DbManagerTestCase
+{
+    protected static $driverName = 'sqlite';
+
+    protected static $sqliteDb;
+
+    public static function createConnection()
+    {
+        // sqlite db is in memory so it can not be reused
+        if (static::$sqliteDb === null) {
+            static::$sqliteDb = parent::createConnection();
+        }
+
+        return static::$sqliteDb;
+    }
+}
