93b261559cc43091c998509b955c20fac55ff665
Alexey Rogachev <arogachev90@gmail.com>
2023-04-13 14:08:47 +0600

    Add mssql

---
+++ tests/Mssql/AssignmentsStorageTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac\Db\Tests\Mssql;
+
+class AssignmentsStorageTest extends \Yiisoft\Rbac\Db\Tests\Base\AssignmentsStorageTest
+{
+    use MssqlTrait;
+}
