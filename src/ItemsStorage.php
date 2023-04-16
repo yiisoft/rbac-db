410266546a0a39b5754d42dd06a1539eee4b7d21
Alexey Rogachev <arogachev90@gmail.com>
2023-04-16 11:06:12 +0600

    Fix remaining failing Oracle tests

--- src/ItemsStorage.php
+++ src/ItemsStorage.php
@@ -397,7 +397,10 @@ final class ItemsStorage implements ItemsStorageInterface
                         ->distinct()
                         ->from($itemsStorage->childrenTableName),
                 ])
-                ->leftJoin(['parent_items' => $itemsStorage->tableName], 'parent_items.name = parents.parent')
+                ->leftJoin(
+                    ['parent_items' => $itemsStorage->tableName],
+                    '{{%parent_items}}.[[name]] = {{%parents}}.[[parent]]',
+                )
                 ->where(['parent_items.type' => $type]);
             $childrenSubQuery = (new Query($database))
                 ->select('children.child')
@@ -407,7 +410,10 @@ final class ItemsStorage implements ItemsStorageInterface
                         ->distinct()
                         ->from($itemsStorage->childrenTableName),
                 ])
-                ->leftJoin(['child_items' => $itemsStorage->tableName], 'child_items.name = children.child')
+                ->leftJoin(
+                    ['child_items' => $itemsStorage->tableName],
+                    '{{%child_items}}.[[name]] = {{%children}}.[[child]]',
+                )
                 ->where(['child_items.type' => $type]);
             $database
                 ->createCommand()
