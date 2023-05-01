fd329398a9f7b3a7c92b9a7288d6490f559763a4
Alexey Rogachev <arogachev90@gmail.com>
2023-05-01 14:48:30 +0600

    Add base code to schema manager

--- src/SchemaManager.php
+++ src/SchemaManager.php
@@ -6,12 +6,24 @@ namespace Yiisoft\Rbac\Db;
 
 use InvalidArgumentException;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Rbac\SchemaManagerInterface;
-use Yiisoft\Rbac\SchemaManagerTrait;
 
-final class SchemaManager implements SchemaManagerInterface
+final class SchemaManager
 {
-    use SchemaManagerTrait;
+    /**
+     * @var string A name of the table for storing RBAC items (roles and permissions).
+     * @psalm-var non-empty-string
+     */
+    private string $itemsTable;
+    /**
+     * @var string A name of the table for storing RBAC assignments.
+     * @psalm-var non-empty-string
+     */
+    private string $assignmentsTable;
+    /**
+     * @var string A name of the table for storing relations between RBAC items.
+     * @psalm-var non-empty-string
+     */
+    private string $itemsChildrenTable;
 
     /**
      * @param string $itemsTable A name of the table for storing RBAC items (roles and permissions).
@@ -114,5 +126,59 @@ final class SchemaManager implements SchemaManagerInterface
     public function dropTable(string $tableName): void
     {
         $this->database->createCommand()->dropTable($tableName)->execute();
+    }
+
+    public function createAll(bool $force = true): void
+    {
+        if ($force === true) {
+            $this->dropAll();
+        }
+
+        $this->createItemsTable();
+        $this->createItemsChildrenTable();
+        $this->createAssignmentsTable();
+    }
+
+    public function dropAll(): void
+    {
+        $this->dropTable($this->itemsChildrenTable);
+        $this->dropTable($this->assignmentsTable);
+        $this->dropTable($this->itemsTable);
+    }
+
+    public function getItemsTable(): string
+    {
+        return $this->itemsTable;
+    }
+
+    public function getAssignmentsTable(): string
+    {
+        return $this->assignmentsTable;
+    }
+
+    public function getItemsChildrenTable(): string
+    {
+        return $this->itemsChildrenTable;
+    }
+
+    private function initTables(string $itemsTable, string $assignmentsTable, string|null $itemsChildrenTable): void
+    {
+        if ($itemsTable === '') {
+            throw new InvalidArgumentException('Items table name can\'t be empty.');
+        }
+
+        $this->itemsTable = $itemsTable;
+
+        if ($assignmentsTable === '') {
+            throw new InvalidArgumentException('Assignments table name can\'t be empty.');
+        }
+
+        $this->assignmentsTable = $assignmentsTable;
+
+        if ($itemsChildrenTable === '') {
+            throw new InvalidArgumentException('Items children table name can\'t be empty.');
+        }
+
+        $this->itemsChildrenTable = $itemsChildrenTable ?? $this->itemsTable . '_child';
     }
 }
