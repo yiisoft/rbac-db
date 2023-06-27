6442af0af1eda1c6d7082811dc212302a7a730d7
Alexey Rogachev <arogachev90@gmail.com>
2023-06-27 17:32:08 +0600

    Update tests/Pgsql/SchemaManagerTest.php
    Co-authored-by: Sergei Predvoditelev <sergei@predvoditelev.ru>

--- tests/Pgsql/SchemaManagerTest.php
+++ tests/Pgsql/SchemaManagerTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Db\Tests\Pgsql;
 
-class SchemaManagerTest extends \Yiisoft\Rbac\Db\Tests\Base\SchemaManagerTest
+class DbSchemaManagerTest extends \Yiisoft\Rbac\Db\Tests\Base\SchemaManagerTest
 {
     use PgsqlTrait;
 }
