53d61d5e086a5fc0e11dbdf811e3c3ec85e1b347
Alexey Rogachev <arogachev90@gmail.com>
2023-09-13 13:44:26 +0600

    Fix tests for other RDBMS

--- tests/Mssql/SchemaTrait.php
+++ tests/Mssql/SchemaTrait.php
@@ -4,33 +4,41 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Db\Tests\Mssql;
 
+use Yiisoft\Rbac\Db\DbSchemaManager;
+
 trait SchemaTrait
 {
     protected function checkItemsChildrenTable(): void
     {
         parent::checkItemsChildrenTable();
 
-        $this->assertCount(2, $this->getDatabase()->getSchema()->getTableForeignKeys(self::ITEMS_CHILDREN_TABLE));
+        $this->assertCount(
+            2,
+            $this->getDatabase()->getSchema()->getTableForeignKeys(DbSchemaManager::ITEMS_CHILDREN_TABLE),
+        );
         $this->assertForeignKey(
-            table: self::ITEMS_CHILDREN_TABLE,
+            table: DbSchemaManager::ITEMS_CHILDREN_TABLE,
             expectedColumnNames: ['parent'],
-            expectedForeignTableName: self::ITEMS_TABLE,
+            expectedForeignTableName: DbSchemaManager::ITEMS_TABLE,
             expectedForeignColumnNames: ['name'],
             expectedOnUpdate: 'NOACTION',
             expectedOnDelete: 'NOACTION',
         );
         $this->assertForeignKey(
-            table: self::ITEMS_CHILDREN_TABLE,
+            table: DbSchemaManager::ITEMS_CHILDREN_TABLE,
             expectedColumnNames: ['child'],
-            expectedForeignTableName: self::ITEMS_TABLE,
+            expectedForeignTableName: DbSchemaManager::ITEMS_TABLE,
             expectedForeignColumnNames: ['name'],
             expectedOnUpdate: 'NOACTION',
             expectedOnDelete: 'NOACTION',
         );
 
-        $this->assertCount(1, $this->getDatabase()->getSchema()->getTableIndexes(self::ITEMS_CHILDREN_TABLE));
+        $this->assertCount(
+            1,
+            $this->getDatabase()->getSchema()->getTableIndexes(DbSchemaManager::ITEMS_CHILDREN_TABLE),
+        );
         $this->assertIndex(
-            table: self::ITEMS_CHILDREN_TABLE,
+            table: DbSchemaManager::ITEMS_CHILDREN_TABLE,
             expectedColumnNames: ['parent', 'child'],
             expectedIsUnique: true,
             expectedIsPrimary: true,
--- tests/Sqlite/SchemaTrait.php
+++ tests/Sqlite/SchemaTrait.php
@@ -4,23 +4,31 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Db\Tests\Sqlite;
 
+use Yiisoft\Rbac\Db\DbSchemaManager;
+
 trait SchemaTrait
 {
     protected function checkItemsChildrenTable(): void
     {
         parent::checkItemsChildrenTable();
 
-        $this->assertCount(1, $this->getDatabase()->getSchema()->getTableForeignKeys('yii_rbac_item_child'));
+        $this->assertCount(
+            1,
+            $this->getDatabase()->getSchema()->getTableForeignKeys(DbSchemaManager::ITEMS_CHILDREN_TABLE),
+        );
         $this->assertForeignKey(
-            table: 'yii_rbac_item_child',
+            table: DbSchemaManager::ITEMS_CHILDREN_TABLE,
             expectedColumnNames: ['parent', 'child'],
-            expectedForeignTableName: 'yii_rbac_item',
+            expectedForeignTableName: DbSchemaManager::ITEMS_TABLE,
             expectedForeignColumnNames: ['name', 'name'],
         );
 
-        $this->assertCount(1, $this->getDatabase()->getSchema()->getTableIndexes('yii_rbac_item_child'));
+        $this->assertCount(
+            1,
+            $this->getDatabase()->getSchema()->getTableIndexes(DbSchemaManager::ITEMS_CHILDREN_TABLE),
+        );
         $this->assertIndex(
-            table: 'yii_rbac_item_child',
+            table: DbSchemaManager::ITEMS_CHILDREN_TABLE,
             expectedColumnNames: ['parent', 'child'],
             expectedIsUnique: true,
             expectedIsPrimary: true,
