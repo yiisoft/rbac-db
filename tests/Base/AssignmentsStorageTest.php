0b4999014ca956f281ab8adc8390223b209ac8d8
Alexey Rogachev <arogachev90@gmail.com>
2023-11-22 13:03:31 +0600

    Fix usage of `createdAt` and `AssignmentsStorageTestTrait`

--- tests/Base/AssignmentsStorageTest.php
+++ tests/Base/AssignmentsStorageTest.php
@@ -7,35 +7,64 @@ namespace Yiisoft\Rbac\Db\Tests\Base;
 use Yiisoft\Rbac\AssignmentsStorageInterface;
 use Yiisoft\Rbac\Db\AssignmentsStorage;
 use Yiisoft\Rbac\Db\DbSchemaManager;
+use Yiisoft\Rbac\Db\ItemsStorage;
+use Yiisoft\Rbac\ItemsStorageInterface;
 use Yiisoft\Rbac\Tests\Common\AssignmentsStorageTestTrait;
 
 abstract class AssignmentsStorageTest extends TestCase
 {
-    use AssignmentsStorageTestTrait;
+    use AssignmentsStorageTestTrait {
+        setUp as protected traitSetUp;
+        tearDown as protected traitTearDown;
+    }
 
-    protected function populateDatabase(): void
+    protected function setUp(): void
     {
-        $fixtures = $this->getFixtures();
+        parent::setUp();
+        $this->traitSetUp();
+    }
 
+    protected function tearDown(): void
+    {
+        $this->traitTearDown();
+        parent::tearDown();
+    }
+
+    protected function populateItemsStorage(): void
+    {
         $this->getDatabase()
             ->createCommand()
             ->batchInsert(
                 DbSchemaManager::ITEMS_TABLE,
                 ['name', 'type', 'createdAt', 'updatedAt'],
-                $fixtures['items'],
+                $this->getFixtures()['items'],
             )
             ->execute();
+    }
+
+    protected function populateAssignmentsStorage(): void
+    {
         $this->getDatabase()
             ->createCommand()
             ->batchInsert(
                 DbSchemaManager::ASSIGNMENTS_TABLE,
                 ['itemName', 'userId', 'createdAt'],
-                $fixtures['assignments'],
+                $this->getFixtures()['assignments'],
             )
             ->execute();
     }
 
-    private function getStorage(): AssignmentsStorageInterface
+    protected function populateDatabase(): void
+    {
+        // Skip
+    }
+
+    protected function createItemsStorage(): ItemsStorageInterface
+    {
+        return new ItemsStorage($this->getDatabase());
+    }
+
+    protected function createAssignmentsStorage(): AssignmentsStorageInterface
     {
         return new AssignmentsStorage($this->getDatabase());
     }
