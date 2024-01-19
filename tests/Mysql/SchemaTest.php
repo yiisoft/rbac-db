ac609945a219a04f73ff59cafad63cc3370452fc
Alexey Rogachev <arogachev90@gmail.com>
2024-01-19 17:24:33 +0600

    Adjust tests for other RDBMS 2

--- tests/Mysql/SchemaTest.php
+++ tests/Mysql/SchemaTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Db\Tests\Mysql;
 
-class SchemaTest extends \Yiisoft\Rbac\Db\Tests\Base\DbSchemaManagerTest
+class SchemaTest extends \Yiisoft\Rbac\Db\Tests\Base\SchemaTest
 {
     use DatabaseTrait;
     use SchemaTrait;
