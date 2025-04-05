1c4cd771216426f4e3142038f3e22061e503de93
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-04-05 15:33:57 +0300

    fix

--- tests/Base/ManagerWithDbItemsAndAssignmentsTest.php
+++ tests/Base/ManagerWithDbItemsAndAssignmentsTest.php
@@ -8,27 +8,9 @@ use Yiisoft\Rbac\AssignmentsStorageInterface;
 use Yiisoft\Rbac\Db\AssignmentsStorage;
 use Yiisoft\Rbac\Db\ItemsStorage;
 use Yiisoft\Rbac\ItemsStorageInterface;
-use Yiisoft\Rbac\Tests\Common\ManagerLogicTestTrait;
 
 abstract class ManagerWithDbItemsAndAssignmentsTest extends ManagerTest
 {
-    use ManagerLogicTestTrait {
-        setUp as protected traitSetUp;
-        tearDown as protected traitTearDown;
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->traitSetUp();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        $this->traitTearDown();
-    }
-
     protected function createItemsStorage(): ItemsStorageInterface
     {
         return new ItemsStorage($this->getDatabase());
--- tests/Base/ManagerWithDbItemsTest.php
+++ tests/Base/ManagerWithDbItemsTest.php
@@ -6,28 +6,10 @@ namespace Yiisoft\Rbac\Db\Tests\Base;
 
 use Yiisoft\Rbac\Db\ItemsStorage;
 use Yiisoft\Rbac\ItemsStorageInterface;
-use Yiisoft\Rbac\Tests\Common\ManagerLogicTestTrait;
 
 abstract class ManagerWithDbItemsTest extends ManagerTest
 {
-    use ManagerLogicTestTrait {
-        setUp as protected traitSetUp;
-        tearDown as protected traitTearDown;
-    }
-
     protected static array $migrationsSubfolders = ['items'];
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->traitSetUp();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        $this->traitTearDown();
-    }
 
     protected function createItemsStorage(): ItemsStorageInterface
     {
