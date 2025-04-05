1dc01cc4398dea50eb9a5f28a979e50db954829b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-04-05 15:31:32 +0300

    fix

--- tests/Base/ManagerWithDbAssignmentsTest.php
+++ tests/Base/ManagerWithDbAssignmentsTest.php
@@ -6,28 +6,10 @@ namespace Yiisoft\Rbac\Db\Tests\Base;
 
 use Yiisoft\Rbac\AssignmentsStorageInterface;
 use Yiisoft\Rbac\Db\AssignmentsStorage;
-use Yiisoft\Rbac\Tests\Common\ManagerLogicTestTrait;
 
 abstract class ManagerWithDbAssignmentsTest extends ManagerTest
 {
-    use ManagerLogicTestTrait {
-        setUp as protected traitSetUp;
-        tearDown as protected traitTearDown;
-    }
-
     protected static array $migrationsSubfolders = ['assignments'];
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->traitSetUp();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        $this->traitTearDown();
-    }
 
     protected function createAssignmentsStorage(): AssignmentsStorageInterface
     {
